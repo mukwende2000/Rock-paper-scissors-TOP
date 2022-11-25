738a68301c241266147e4904c4a05515c1dc5703
Mukwende <mukwende2000@gmail.com>
2022-11-25 08:33:20 +0200

    Added ui to the game

--- script.js
+++ script.js
@@ -1,42 +1,61 @@
-let playerScore = 0, computerScore = 0, round = 0
-const choices = ['Rock', 'Paper', 'Scissors']
+// p1 stands for player one
+// Therefore p1Score is player one score
+const textResult = document.querySelector('.text-result')
+const p1Score = document.querySelector('.p1 .score')
+const p2Score = document.querySelector('.p2 .score')
+const gestures = document.querySelectorAll('.gameplay img')
+const round = document.querySelector('.round')
+
+gestures.forEach(gesture => {
+    gesture.addEventListener('click', playRound)
+})
+
+const choices = ['rock', 'paper', 'scissors']
 
 function getComputerChoice() {
     return choices[Math.floor(Math.random() * choices.length)]
 }
 
-function playRound(computerSelection, playerSelection) {
+function playRound(e) {
+    const playerSelection = e.target.getAttribute('src').slice(0, e.target.getAttribute('src').indexOf('.'))
+    const computerSelection = getComputerChoice()
     if(computerSelection === playerSelection) {
-        console.log('Its a draw')
-        playerScore++
-        computerScore++
+        textResult.textContent = 'Its a draw'
+        round.textContent++
+        game()
         
-    } else if(computerSelection === 'Rock'.toLowerCase() && playerSelection === 'Scissors' || 
-    computerSelection === 'Paper' && playerSelection === 'Rock' ||
-    computerSelection === 'Scissors' && playerSelection === 'Paper') {
-        console.log(`You lose, ${computerSelection} beats ${playerSelection}`)
-        computerScore++
+    } else if(computerSelection === 'rock' && playerSelection === 'scissors' || 
+    computerSelection === 'paper' && playerSelection === 'rock' ||
+    computerSelection === 'scissors' && playerSelection === 'paper') {
+        textResult.textContent =`You lose, ${computerSelection} beats ${playerSelection}`
+        p2Score.textContent++
+        round.textContent++
+        game()
         
     } else {
-        console.log(`You win, ${playerSelection} beat ${computerSelection}`)
-        playerScore++
+        textResult.textContent = `You win, ${playerSelection} beat ${computerSelection}`
+        p1Score.textContent++
+        round.textContent++
+        game()
     }
-    console.log(`Player Score: ${playerScore}`)
-    console.log(`computer Score: ${computerScore}`)
 }
 
 function game() {
-    for(i = 0; i < 5; i++) {
-        const playerSelection = prompt('Enter your choice')
-        playRound(getComputerChoice(), playerSelection)
-    }
-    if(playerScore === computerScore) {
-        console.log('You tied with the computer')
-    } else if (playerScore > computerScore) {
-        console.log('You won, you beat the computer') 
+    if(round.textContent < 6) return
+    if(p1Score.textContent === p2Score.textContent) {
+        textResult.textContent = 'You tied with the computer'
+        round.textContent = 5
+    } else if (p1Score.textContent > p2Score.textContent) {
+        textResult.textContent = 'You won, you beat the computer' 
+        round.textContent = 5
     } else {
-        console.log('You lost, the computer beat you')
+        textResult.textContent = 'You lost, the computer beat you'
+        round.textContent = 5
     }
+    setTimeout(() => {
+        p2Score.textContent = 0
+        p1Score.textContent = 0
+        textResult.textContent = 'Try and beat the computer'
+        round.textContent = 1
+    }, 2000);
 }
-
-game()
